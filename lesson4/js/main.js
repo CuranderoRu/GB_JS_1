432a61659453402f3e379dacffaf42848768e0d8
Sergei Lap <sergei.ogarkov@sogarkov.ru>
2020-09-26 19:11:30 +0300

    Lesson 4 init

---
+++ lesson4/js/main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+function init() {
+    console.log("Homework 4");
+//    ex1(); //answer for exercise 1
+//    ex2(); //answer for exercise 2
+//    ex3(); //answer for exercise 3
+//    ex4(); //answer for exercise 4
+//    ex5(); //answer for exercise 5
+//    ex6(); //answer for exercise 6
+//    ex7(); //answer for exercise 7
+//    ex8(); //answer for exercise 8
+}
