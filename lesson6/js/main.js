79e5bffdaa2254394c61a324cd94e0f5eed6ae1e
Sergei Mono <sergei.ogarkov@sogarkov.ru>
2020-10-06 17:53:27 +0300

    Ex1, 3 done

--- lesson6/js/main.js
+++ lesson6/js/main.js
@@ -1,10 +1,149 @@
 'use strict';
 
+const picArray = getPicsArray();
+let currentIndex = -1;
+
+function getPicsArray() {
+    return [
+        {
+            image_id: "incorrect",
+            path: "/img/small/08.jpg"
+        },
+        {
+            image_id: "img1",
+            path: "/img/small/01.jpg"
+        },
+        {
+            image_id: "img2",
+            path: "/img/small/02.jpg"
+        },
+        {
+            image_id: "incorrect2",
+            path: "/img/small/09.jpg"
+        },
+        {
+            image_id: "img3",
+            path: "/img/small/03.jpg"
+        },
+    ];
+}
+
+function removeShadows(){
+    let div;
+    for (var i = 0; i < picArray.length; i++) {
+        div = document.querySelector("#" + picArray[i].image_id);
+        if (div === null) {
+            continue;
+        }
+        div.classList.remove("boxShadow");
+    }
+}
+
+function setPic(imgObjIndex) {
+    if (imgObjIndex === -1) return;
+    let imgObj = picArray[imgObjIndex];
+    currentIndex = imgObjIndex;
+    let bigPic = document.querySelector("#big-picture");
+    let path = imgObj.path.replace("/small/", "/big/");
+    //здесь решение первого задания: в случае отсутствия картинки, используем малый экземпляр
+    let img = new Image();
+    img.src = '.' + path;
+    img.onload = function () {
+        bigPic.src = '.' + path;
+    };
+    img.onerror = function () {
+        bigPic.src = '.' + imgObj.path;
+    };
+    let div = document.querySelector("#" + picArray[imgObjIndex].image_id);
+    removeShadows();
+    div.classList.add("boxShadow");
+}
+
+function onClickHandler(e) {
+    setPic(picArray.findIndex(item => item.image_id == e.target.id));
+}
+
+function onClickBackward(e) {
+    let imgObjIndex = -1;
+    let div;
+    for (var i = currentIndex-1; i >= 0; i--) {
+        div = document.querySelector("#" + picArray[i].image_id);
+        if (div === null) {
+            continue;
+        }
+        imgObjIndex = i;
+        break;
+    }
+    if (imgObjIndex == -1) {
+        for (var i = picArray.length - 1; i >= 0; i--) {
+            div = document.querySelector("#" + picArray[i].image_id);
+            if (div === null) {
+                continue;
+            }
+            imgObjIndex = i;
+            break;
+        }
+    }
+    setPic(imgObjIndex);
+}
+
+function onClickForward(e) {
+    let imgObjIndex = -1;
+    let div;
+    for (var i = currentIndex+1; i < picArray.length; i++) {
+        div = document.querySelector("#" + picArray[i].image_id);
+        if (div === null) {
+            continue;
+        }
+        imgObjIndex = i;
+        break;
+    }
+    if (imgObjIndex == -1) {
+        for (var i = 0; i < picArray.length; i++) {
+            div = document.querySelector("#" + picArray[i].image_id);
+            if (div === null) {
+                continue;
+            }
+            imgObjIndex = i;
+            break;
+        }
+    }
+    setPic(imgObjIndex);
+}
+
+function addListeners() {
+    let div;
+    for (var i = 0; i < picArray.length; i++) {
+        div = document.querySelector("#" + picArray[i].image_id);
+        if (div === null) {
+            continue;
+        }
+        div.addEventListener('click', onClickHandler)
+        if (currentIndex == -1) { //присваиваем индекс первого валидного элемента массива
+            currentIndex = i;
+        }
+    }
+    document.querySelector("#move-backward").addEventListener('click', onClickBackward);
+    document.querySelector("#move-forward").addEventListener('click', onClickForward);
+
+}
+
+function fillImages() {
+    let div;
+    for (var i = 0; i < picArray.length; i++) {
+        div = document.querySelector("#" + picArray[i].image_id);
+        if (div === null) {
+            continue;
+        }
+        div.style.backgroundImage = 'url(".' + picArray[i].path + '")';
+    }
+
+}
+
 function init() {
     console.log("Homework 6, ex 1");
 
-    ex1(); //С помощью цикла while вывести все простые числа в промежутке от 0 до 100
-    ex3(); //Товары в корзине хранятся в массиве. Задачи
-    ex4(); //Вывести с помощью цикла for числа от 0 до 9, не используя тело цикла
-    ex5(); //Нарисовать пирамиду с помощью console.log, как показано на рисунке, только у вашей пирамиды должно быть 20 рядов, а не 5
+    fillImages();
+    addListeners();
+    setPic(currentIndex);
 }
